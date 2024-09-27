54ebbf3881122afa73a48b2de477f70633133f03
BrunoSerpa <serpabruno2005@gmail.com>
2024-09-27 09:09:42 -0300

    Inseri o código que mostra o ip

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import os from "os";
 
 import { ProdutoRouter } from "./endpoints/Produtos";
 import { CadastroRouter } from "./endpoints/Cadastro";
@@ -14,10 +15,24 @@ app.use(require("body-parser").urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.json());
 
+function getLocalIPAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]!) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return "localhost";
+}
+
 app.listen(
   PORT,
   function () {
+    const ip = getLocalIPAddress();
     console.log(`API aberta na porta ${PORT}`);
+    console.log(`Link Online: http://${ip}:${PORT}`);
   }
 );
 
